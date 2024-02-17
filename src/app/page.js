3774171b0844751ce29fafedf6cef814634f61b5
Mu-Solomon <musolomoni@gmail.com>
2024-02-18 02:18:43 +0300

    I wanna complete

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ import React from "react";
 export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col h-screen">
-      <App />
+      {/*    <App /> */}
+      <div>Home</div>
     </div>
   );
 }
